bfa3eb55d4ce6da70d7ae1abd3fde7d80a63378f
Григораш Виталий Юрьевич <v.grigorash@spbu.ru>
2022-03-10 16:11:13 +0300

    add tabs to Users section

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -13,10 +13,22 @@ function Users(props) {
     const [roleOptionsContainerValue, setRoleOptionsContainerValue] = useState('Оператор');
     const [showResultsFrom, setShowResultsFrom] = useState(0);
     const [resultsShow, setResultsShow] = useState(10);
+    const [isAddUserTabActive, setAddUserTabActive] = useState(true);
+    const [isListTabActive, setListTabActive] = useState(false);
 
     useEffect(() => {
         handleMobileHeaderNavText('Пользователи');
     });
+
+    function onAddUserTabClick() {
+        setListTabActive(false);
+        setAddUserTabActive(true);
+    }
+
+    function onListTabClick() {
+        setAddUserTabActive(false);
+        setListTabActive(true);
+    }
 
     function handleShowResultsFrom(value) {
         setShowResultsFrom(value);
@@ -44,86 +56,92 @@ function Users(props) {
                 title='Пользователи'
             />
             <h1 className='users__heading'>Пользователи</h1>
-            <div className='users__add-user-container'>
-                <p className='users__add-user-heading'>Добавить пользователя</p>
-                <div className='users__add-user-inputs-main-container'>
-                    <div className='users__add-user-inputs-container'>
-                        <div className='users__add-user-input-container'>
-                            <p className='users__add-user-input-label'>Фамилия</p>
-                            <input type="text" placeholder='' className='users__add-user-input' />
-                        </div>
-                        <div className='users__add-user-input-container'>
-                            <p className='users__add-user-input-label'>Имя</p>
-                            <input type="text" placeholder='' className='users__add-user-input' />
-                        </div>
-                        <div className='users__add-user-input-container'>
-                            <p className='users__add-user-input-label'>Отчество</p>
-                            <input type="text" placeholder='' className='users__add-user-input' />
-                        </div>
-                    </div>
-                    <div className='users__add-user-inputs-container'>
-                        <div className='users__add-user-input-container'>
-                            <p className='users__add-user-input-label'>Логин</p>
-                            <input type="text" placeholder='' className='users__add-user-input' />
-                        </div>
-                        <div className='users__add-user-input-container'>
-                            <p className='users__add-user-input-label'>Почта</p>
-                            <input type="text" placeholder='' className='users__add-user-input' />
-                        </div>
-                        <div className='users__add-user-input-container'>
-                            <p className='users__add-user-input-label'>Роль</p>
-                            <div className='users__add-user-role-container' onClick={handleRoleOptionsContainerOpen}>
-                                <p className='users__add-user-role-text'>{roleOptionsContainerValue}</p>
-                                <div className='users__add-user-role-arrow-icon' />
-                                <div className={`users__add-user-role-options-container ${isRoleOptionsContainerOpen && 'users__add-user-role-options-container_active'}`}>
-                                    <div className='users__add-user-role-option-container'>
-                                        <p className='users__add-user-role-option' onClick={() => onRoleOptionClick('Администратор')}>Администратор</p>
-                                    </div>
-                                    <div className='users__add-user-role-option-container'>
-                                        <p className='users__add-user-role-option' onClick={() => onRoleOptionClick('Оператор')}>Оператор</p>
+            <div className='users__container'>
+                <div className='users__tabs-container'>
+                    <p className={`users__tab ${isAddUserTabActive && 'users__tab_active'}`} onClick={onAddUserTabClick}>Добавить пользователя</p>
+                    <p className={`users__tab ${isListTabActive && 'users__tab_active'}`} onClick={onListTabClick}>Список</p>
+                </div>
+                {isAddUserTabActive && (
+                    <div className='users__add-user-container'>
+                        <div className='users__add-user-inputs-main-container'>
+                            <div className='users__add-user-inputs-container'>
+                                <div className='users__add-user-input-container'>
+                                    <p className='users__add-user-input-label'>Фамилия</p>
+                                    <input type="text" placeholder='' className='users__add-user-input' />
+                                </div>
+                                <div className='users__add-user-input-container'>
+                                    <p className='users__add-user-input-label'>Имя</p>
+                                    <input type="text" placeholder='' className='users__add-user-input' />
+                                </div>
+                                <div className='users__add-user-input-container'>
+                                    <p className='users__add-user-input-label'>Отчество</p>
+                                    <input type="text" placeholder='' className='users__add-user-input' />
+                                </div>
+                            </div>
+                            <div className='users__add-user-inputs-container'>
+                                <div className='users__add-user-input-container'>
+                                    <p className='users__add-user-input-label'>Логин</p>
+                                    <input type="text" placeholder='' className='users__add-user-input' />
+                                </div>
+                                <div className='users__add-user-input-container'>
+                                    <p className='users__add-user-input-label'>Почта</p>
+                                    <input type="text" placeholder='' className='users__add-user-input' />
+                                </div>
+                                <div className='users__add-user-input-container'>
+                                    <p className='users__add-user-input-label'>Роль</p>
+                                    <div className='users__add-user-role-container' onClick={handleRoleOptionsContainerOpen}>
+                                        <p className='users__add-user-role-text'>{roleOptionsContainerValue}</p>
+                                        <div className='users__add-user-role-arrow-icon' />
+                                        <div className={`users__add-user-role-options-container ${isRoleOptionsContainerOpen && 'users__add-user-role-options-container_active'}`}>
+                                            <div className='users__add-user-role-option-container'>
+                                                <p className='users__add-user-role-option' onClick={() => onRoleOptionClick('Администратор')}>Администратор</p>
+                                            </div>
+                                            <div className='users__add-user-role-option-container'>
+                                                <p className='users__add-user-role-option' onClick={() => onRoleOptionClick('Оператор')}>Оператор</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <button type='button' className='users__user-add-button'>Добавить</button>
                     </div>
-                </div>
-                <button type='button' className='users__user-add-button'>Добавить</button>
-            </div>
-            <div className='users__table-container'>
-                <p className='users__table-heading'>Список пользователей</p>
-                <div className='users__table'>
-                    <div className='users__table-rows users__table-rows_heading'>
-                        <p className='users__table-last-name'>Фамилия</p>
-                        <p className='users__table-first-name'>Имя</p>
-                        <p className='users__table-middle-name'>Отчество</p>
-                        <p className='users__table-login'>Логин</p>
-                        <p className='users__table-password'>Пароль</p>
-                        <p className='users__table-role'>Роль</p>
-                    </div>
-                    {UsersListData !== null ? (
-                        <>
-                            {UsersListData.slice(showResultsFrom, resultsShow).map((list) => (
-                                <div key={list.id} className='users__table-rows'>
-                                    <p className='users__table-last-name'>{list.lastName}</p>
-                                    <p className='users__table-first-name'>{list.firstName}</p>
-                                    <p className='users__table-middle-name'>{list.middleName}</p>
-                                    <p className='users__table-login'>{list.login}</p>
-                                    <p className='users__table-password'>{list.password}</p>
-                                    <p className='users__table-role'>{list.role}</p>
-                                </div>
-                            ))}
-                        </>
-                    ) : (
-                        <div className='users__table-rows'>
-                            <p className='users__no-results-text'>Нет данных для отображения</p>
+                )}
+                {isListTabActive && (
+                    <div className='users__table-container'>
+                        <div className='users__table-rows users__table-rows_heading'>
+                            <p className='users__table-last-name'>Фамилия</p>
+                            <p className='users__table-first-name'>Имя</p>
+                            <p className='users__table-middle-name'>Отчество</p>
+                            <p className='users__table-login'>Логин</p>
+                            <p className='users__table-password'>Пароль</p>
+                            <p className='users__table-role'>Роль</p>
                         </div>
-                    )}
-                    <TablePagination
-                        sortList={UsersListData}
-                        handleShowResultsFrom={handleShowResultsFrom}
-                        handleResultsShow={handleResultsShow}
-                    />
-                </div>
+                        {UsersListData !== null ? (
+                            <>
+                                {UsersListData.slice(showResultsFrom, resultsShow).map((list) => (
+                                    <div key={list.id} className='users__table-rows'>
+                                        <p className='users__table-last-name'>{list.lastName}</p>
+                                        <p className='users__table-first-name'>{list.firstName}</p>
+                                        <p className='users__table-middle-name'>{list.middleName}</p>
+                                        <p className='users__table-login'>{list.login}</p>
+                                        <p className='users__table-password'>{list.password}</p>
+                                        <p className='users__table-role'>{list.role}</p>
+                                    </div>
+                                ))}
+                            </>
+                        ) : (
+                            <div className='users__table-rows'>
+                                <p className='users__no-results-text'>Нет данных для отображения</p>
+                            </div>
+                        )}
+                        <TablePagination
+                            sortList={UsersListData}
+                            handleShowResultsFrom={handleShowResultsFrom}
+                            handleResultsShow={handleResultsShow}
+                        />
+                    </div>
+                )}
             </div>
         </div>
     );
