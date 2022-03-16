7d60841a785c40c86b87440a80935987df5461e0
Vitalii Grigorash <v.grigorash@spbu.ru>
2022-03-16 17:13:40 +0300

    EDEMES22-5 refactor BoxRegistrationRoute work method

--- src/components/BoxRegistration/BoxRegistration.jsx
+++ src/components/BoxRegistration/BoxRegistration.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { YMaps, Map } from 'react-yandex-maps';
 import BoxRegistrationExhibits from '../BoxRegistrationExhibits/BoxRegistrationExhibits';
-import BoxRegistrationRoute from '../BoxRegistrationRoute/BoxRegistrationRoute';
 import BoxRegistrationQrCode from '../BoxRegistrationQrCode/BoxRegistrationQrCode';
 import { Validation } from '../../utils/Validation';
 
@@ -15,6 +15,20 @@ function BoxRegistration(props) {
     const [isBoxRegistrationExhibitsActive, setBoxRegistrationExhibitsActive] = useState(false);
     const [isBoxRegistrationRouteActive, setBoxRegistrationRouteActive] = useState(false);
     const [isBoxRegistrationQrCodeActive, setBoxRegistrationQrCodeActive] = useState(false);
+    const [isTrainActive, setTrainActive] = useState(false);
+    const [isAirplaneActive, setAirplaneActive] = useState(true);
+    const [isBusActive, setBusActive] = useState(false);
+    const [isCarActive, setCarActive] = useState(false);
+    const [departureMethod, setDepartureMethod] = useState('airplane');
+
+    const from = Validation();
+    const to = Validation();
+
+    const route = {
+        departureMethod: departureMethod,
+        from: from.value,
+        to: to.value
+    }
 
     const boxName = Validation();
     const length = Validation();
@@ -38,17 +52,6 @@ function BoxRegistration(props) {
         temperature: temperature.value
     }
 
-    function clearAllBoxRegistrationInputs() {
-        boxName.setValue('');
-        length.setValue('');
-        width.setValue('');
-        height.setValue('');
-        weight.setValue('');
-        price.setValue('');
-        humidity.setValue('');
-        temperature.setValue('');
-    }
-
     const mobileHeading = (() => {
         if (isBoxRegistrationGeneralInformationActive) {
             return 'Общая информация';
@@ -64,6 +67,38 @@ function BoxRegistration(props) {
     useEffect(() => {
         handleMobileHeaderNavText('Регистрация ящика');
     });
+
+    function handleTrainActive() {
+        setAirplaneActive(false);
+        setBusActive(false);
+        setCarActive(false);
+        setTrainActive(true);
+        setDepartureMethod('train');
+    }
+
+    function handleAirplaneActive() {
+        setTrainActive(false);
+        setBusActive(false);
+        setCarActive(false);
+        setAirplaneActive(true);
+        setDepartureMethod('airplane');
+    }
+
+    function handleBusActive() {
+        setTrainActive(false);
+        setAirplaneActive(false);
+        setCarActive(false);
+        setBusActive(true);
+        setDepartureMethod('bus');
+    }
+
+    function handleCarActive() {
+        setTrainActive(false);
+        setAirplaneActive(false);
+        setBusActive(false);
+        setCarActive(true);
+        setDepartureMethod('car');
+    }
 
     function onGeneralInformationTabClick() {
         setBoxRegistrationQrCodeActive(false);
@@ -119,13 +154,30 @@ function BoxRegistration(props) {
         } else if (isBoxRegistrationRouteActive) {
             setBoxRegistrationRouteActive(false);
             setBoxRegistrationQrCodeActive(true);
+            console.log(route);
         }
+    }
+
+    function clearAllBoxRegistrationInputs() {
+        boxName.setValue('');
+        length.setValue('');
+        width.setValue('');
+        height.setValue('');
+        weight.setValue('');
+        price.setValue('');
+        humidity.setValue('');
+        temperature.setValue('');
+        from.setValue('');
+        to.setValue('');
+        handleAirplaneActive();
     }
 
     function onRegisterButtonClick() {
         clearAllBoxRegistrationInputs();
         const dataToRegister = {
-            generalInformation: generalInformation
+            generalInformation: generalInformation,
+            route: route,
+            qrCode: "qrCode.jpg"
         }
         console.log(dataToRegister);
     }
@@ -246,9 +298,51 @@ function BoxRegistration(props) {
                     )
                 }
                 {isBoxRegistrationRouteActive &&
-                    (
-                        <BoxRegistrationRoute />
-                    )
+                    <div className='box-registration-route'>
+                        <div className='box-registration-route__form-container'>
+                            <div className='box-registration-route__icons-container'>
+                                <div className={`box-registration-route__icon-train ${isTrainActive && 'box-registration-route__icon-train_active'}`} onClick={handleTrainActive} />
+                                <div className={`box-registration-route__icon-airplane ${isAirplaneActive && 'box-registration-route__icon-airplane_active'}`} onClick={handleAirplaneActive} />
+                                <div className={`box-registration-route__icon-bus ${isBusActive && 'box-registration-route__icon-bus_active'}`} onClick={handleBusActive} />
+                                <div className={`box-registration-route__icon-car ${isCarActive && 'box-registration-route__icon-car_active'}`} onClick={handleCarActive} />
+                            </div>
+                            <div className='box-registration-route__inputs-container'>
+                                <div className='box-registration-route__input-container'>
+                                    <p className='box-registration-route__input-label'>Откуда</p>
+                                    <input
+                                        type="text"
+                                        className='box-registration-route__input'
+                                        name="from"
+                                        placeholder=''
+                                        value={from.value}
+                                        onChange={from.onChange}
+                                    />
+                                </div>
+                                <div className='box-registration-route__input-container'>
+                                    <p className='box-registration-route__input-label'>Куда</p>
+                                    <input
+                                        type="text"
+                                        className='box-registration-route__input'
+                                        name="to"
+                                        placeholder=''
+                                        value={to.value}
+                                        onChange={to.onChange}
+                                    />
+                                </div>
+                            </div>
+                            <p className='box-registration-route__travel-time'>Расчетное время в пути 3 ч 35 мин</p>
+                        </div>
+                        <YMaps>
+                            <div className="box-registration-route__map-container">
+                                <Map
+                                    defaultState={{ center: [59.939098, 30.315868], zoom: 8 }}
+                                    width={"100%"}
+                                    height={"100%"}
+                                >
+                                </Map>
+                            </div>
+                        </YMaps>
+                    </div>
                 }
                 {isBoxRegistrationQrCodeActive &&
                     (
